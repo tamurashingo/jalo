4e45d690f4560dc40ca69e315d1b3d2dd10f90b8
Tamura Shingo <tamura.shingo@gmail.com>
2014-03-12 23:03:59 +0900

    add integration test

---
+++ integration/src/boot0002/src/main/java/com/github/tamurashingo/jalo/integration/TestMain.java
@@ -0,0 +1,11 @@
+package com.github.tamurashingo.jalo.integration;
+
+public class TestMain {
+
+    public static final String version = "0.2";
+
+    public static void main(String...args) {
+        System.out.println("hello world");
+    }
+
+}
